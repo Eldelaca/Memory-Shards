b1ff1311dea680dc2e69114e18d753e9a88b47d7
Eldelaca <albusfgallardo@gmail.com>
2025-04-08 04:00:08 +0100

    added in player save and load pos manager and script

--- Assets/Script/Player/SaveLoad/PlayerSaveManager.cs
+++ Assets/Script/Player/SaveLoad/PlayerSaveManager.cs
@@ -1,20 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;  
 
-public class UIManager : MonoBehaviour
+public class PlayerSaveManager : MonoBehaviour
 {
-    public Button saveButton;
-    public Button loadButton;
+    
     public PlayerSaveSystem saveSystem;
     public Transform playerTransform;
     public Transform enemyTransform; 
 
-    void Start()
-    {
-        // Assign button listeners
-        saveButton.onClick.AddListener(SavePosition);
-        loadButton.onClick.AddListener(LoadPosition);
-    }
+   
 
     public void SavePosition()
     {
